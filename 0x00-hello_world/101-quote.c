50f562dc8e28950d8211fac21d26c9ce2d58c68d
rowniee <ngacha.oronnie@gmail.com>
2023-02-16 11:56:43 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	getputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
