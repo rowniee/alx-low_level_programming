19aeb336fc007000c951e4c0718f84b9b5eea24d
rowniee <ngacha.oronnie@gmail.com>
2023-02-18 00:49:24 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - beginnig of the programme
+ * to print base 10 numbers
+ * Return: 0(Succes)
+ */
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+	printf("%d", n);
+		n++;
+}
+return (0);
+}
