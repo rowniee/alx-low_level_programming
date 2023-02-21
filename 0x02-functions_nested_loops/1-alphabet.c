d6d76b14d268beca0e9e5eb07508b127b433c395
rowniee <ngacha.oronnie@gmail.com>
2023-02-21 10:36:33 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_alphabet - prints the lower case alphabets
+ * print alphabets a to z
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a' ; c <= 'z' ; c++)
+{
+	_putchar (c);
+}
+_putchar ('\n');
+}
