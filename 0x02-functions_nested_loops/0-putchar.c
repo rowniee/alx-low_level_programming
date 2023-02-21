8c62f882c93adfda5d51e438bdf38004015f7f84
rowniee <ngacha.oronnie@gmail.com>
2023-02-20 21:06:58 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * main - beginning of the function
+ * print _putchar followed by new line
+ * Return: 0(Success)
+ */
+int main(void)
+{
+int i;
+char s[] = "_putchar\n";
+int len = strlen(s);
+for (i = 0 ; i < len ; i++)
+{
+	putchar (s[i]);
+}
+return (0);
+}
