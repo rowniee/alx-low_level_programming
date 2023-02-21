047ff78a920cac09eee191ae81e9c838847883da
rowniee <ngacha.oronnie@gmail.com>
2023-02-21 13:30:23 -0800

    7-print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _abs - returns -n if n is less than 0 otherwise n
+ * @n: integer to be tested
+ * Return: _abs(n)
+ */
+int _abs(int n)
+{
+if (n >= 0)
+return (n);
+else
+return (-1 * n);
+}
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: integer whose last digit is to be printed
+ * Return: lastdigit
+ */
+int print_last_digit(int n)
+{
+int lastDigit;
+
+lastDigit = _abs((n % 10));
+_putchar(lastDigit + 48);
+return (lastDigit);
+}
