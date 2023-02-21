514eb8a3e32488cfb987e7c183a326109328d7dc
rowniee <ngacha.oronnie@gmail.com>
2023-02-21 12:13:10 -0800

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: value to be checked
+ * Return: 1 if lowercase or uppercase else 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' || c <= 'z' || c >= 'A' || c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
