2863daffc02462d47555a7594baf5914a28b4d08
rowniee <ngacha.oronnie@gmail.com>
2023-02-21 08:25:19 -0800

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include<string.h>
-#include<stdlib.h>
 /**
  * main - beginning of the function
  * print _putchar followed by new line
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#define MAIN_H
+
+int _putchar (char c);
+
+#endif
