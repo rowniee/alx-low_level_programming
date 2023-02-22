1aa68932d52eeda8d4134c8134b40baaf88a3891
rowniee <ngacha.oronnie@gmail.com>
2023-02-21 22:07:12 -0800

    8-24

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the da of jack bauer
+ * Returns: nothing
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0 ; h <= 24 ; h++)
+	{
+		for (m = 0 ; m <= 60 ; m++)
+		{
+			_putchar (h / 10 + '0');
+			_putchar (h % 10 + '0');
+			_putchar (':');
+			_putchar (m / 10 + '0');
+			_putchar (m % 10 + '0');
+			_putchar ('\n');
+		}
+	}
+}
