c7119ccc9c86b636862ad5921665f6884e4a1ff9
rowniee <ngacha.oronnie@gmail.com>
2023-02-23 04:32:06 -0800

    6-print_line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ * the line should end with anew line
+ * @n: number of times the character is printed
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar ('\n');
+	}
+	else
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+			_putchar ('_');
+		}
+		_putchar ('\n');
+	}
+}
+
