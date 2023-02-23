0eff050788145b32075def72c3b55f9d8fa9bdfe
rowniee <ngacha.oronnie@gmail.com>
2023-02-23 07:39:34 -0800

    8-print_square

---
+++ 0x04-more_functions_nested_loops/7-print_.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character should be printed
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar ('\n');
+	}
+	for (i = 0 ; i < n ; i++)
+	{
+		for (j = 0 ; j <= i ; j++)
+		{
+			if (j == i)
+			{
+				_putchar('\\');
+				_putchar ('\n');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+	}
+}
