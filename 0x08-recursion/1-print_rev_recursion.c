7205ae388c601da46a229e20db2dd0f6306ea095
rowniee <ngacha.oronnie@gmail.com>
2023-03-08 05:12:49 -0800

    rev_recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - beginning of my programme
+ * @s: string to be printed
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
