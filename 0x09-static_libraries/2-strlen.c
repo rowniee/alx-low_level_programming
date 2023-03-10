0e4daaae3c53b500b8638de8f4750aebe8d8ea60
rowniee <ngacha.oronnie@gmail.com>
2023-03-10 06:07:22 -0800

    static_lib

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: declaration of *s and paramters for the function _strlen
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+	return (c);
+}
+
