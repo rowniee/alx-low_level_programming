f44c2d0fba30b1cebf7c57a0a6d42ef063e37cef
rowniee <ngacha.oronnie@gmail.com>
2023-03-20 09:38:00 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
-struct dog
+#ifndef DOG_H
+#define DOG_H
+/*
+ * struct dog - structure definition of a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner
+ */
+typedef struct
 {
 	char * name;
 	float age;
 	char * owner;
-};
-
+}dog;
 #endif
