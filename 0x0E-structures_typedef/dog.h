de19761b68c180906935e25f414ca732453bcb7a
rowniee <ngacha.oronnie@gmail.com>
2023-03-21 07:53:55 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
-/*
+/**
  * struct dog - structure definition of a dog
  * @name: dog name
  * @age: dog age
  * @owner: owner
  */
-struct dog
+typedef struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
-};
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
+	char *owner;
+} dog_t;
+
 #endif
