254cb648dd1cec4bac3a600312dcc59302c952f5
rowniee <ngacha.oronnie@gmail.com>
2023-04-08 19:27:30 -0700

    main_h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,7 @@
-#include <stddef.h>
-#include <sys/types.h>
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
+#include <sys/types.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
