85b57c84c73213e4dadd063b2869bd8b8d03895b
Ronnie Ngacha <ngacha.oronnie@gmail.com>
2023-06-13 12:27:36 -0700

    string concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,16 +16,16 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0 ;s1[i] != '\0' ; i++)
+	for (i = 0 ; s1[i] != '\0' ; i++)
 		;
 	for (j = 0 ; s2[j] != '\0' ; j++)
 		;
-	a = malloc( sizeof(char) * (i + j + 1));
+	a = malloc(sizeof(char) * (i + j + 1));
 	if (a == NULL)
 	{
 		return (NULL);
 	}
-	for ( k = 0, l = 0 ; k < ( i + j + 1) ; k++)
+	for (k = 0, l = 0 ; k < (i + j + 1) ; k++)
 	{
 		if (k < i)
 		{
