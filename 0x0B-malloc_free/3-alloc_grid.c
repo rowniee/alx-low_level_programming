9b12fc0c2033d93694197a69e28d024a5f5d5d6a
Ronnie Ngacha <ngacha.oronnie@gmail.com>
2023-06-14 00:11:17 -0700

    2-d array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -6,4 +6,34 @@
  * @height: array of the array
  * Return: 2-d array of integers
  */
+int **alloc_grid(int width, int height)
+{
+	int **grid, c, d;
 
+	if (width < 1 ||  height < 1)
+		return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (c = 0 ; c < height ; c++)
+	{
+		grid[c] = malloc(sizeof(int) * width);
+		if (grid[c] == NULL)
+		{
+			for (c-- ; c >= 0 ; c--)
+			{
+				free(grid[c]);
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
+	c = 0;
+	for (d = 0 ; d < width ; d++)
+	{
+		grid[c][d] = 0;
+	}
+	return (grid);
+}
