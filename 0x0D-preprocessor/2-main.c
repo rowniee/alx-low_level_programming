6d90e454f39583bf4dd6b9cb57287d1ef3ac1722
Ronnie Ngacha <ngacha.oronnie@gmail.com>
2023-06-19 14:54:41 -0700

    2-main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of file of the program
+ *        was compiled from, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
