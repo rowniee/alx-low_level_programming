c5950bb43e064c0e3966bb1917f304be9d3df1d2
Ronnie Ngacha <ngacha.oronnie@gmail.com>
2023-06-21 06:43:59 -0700

    0-print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - Write a function that prints a name
+ * @name: name of the person
+ * @f: paramter for the function pointer
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!f || !name)
+		return;
+	(*f)(name);
+}
